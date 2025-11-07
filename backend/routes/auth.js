019607b569521217165caafeb6a5b87a951c01fe
pr1ynsu <poddarpriyanshukr@gmail.com>
2025-11-08 01:26:48 +0530

    replace native bcrypt with bcryptjs to fix MODULE_NOT_FOUND

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 // backend/routes/auth.js (FINAL RECOMMENDED VERSION)
 
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
